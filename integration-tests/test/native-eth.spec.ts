Tenderly/optimism

feat[integration-tests]: make tests work for prod networks

integration-tests/test/native-eth.spec.ts
@@ -1,7 +1,10 @@
+import { expect } from 'chai'
+
+/* Imports: External */
+import { Wallet, utils, BigNumber } from 'ethers'
 import { predeploys } from '@eth-optimism/contracts'
 
-import { expect } from 'chai'
-import { Wallet, utils, BigNumber } from 'ethers'
+/* Imports: Internal */
 import { Direction } from './shared/watcher-utils'
 
 import {
@@ -9,7 +12,7 @@ import {
   fundUser,
   PROXY_SEQUENCER_ENTRYPOINT_ADDRESS,
 } from './shared/utils'
-import { OptimismEnv } from './shared/env'
+import { OptimismEnv, useDynamicTimeoutForWithdrawals } from './shared/env'
 
 const DEFAULT_TEST_GAS_L1 = 330_000
 const DEFAULT_TEST_GAS_L2 = 1_300_000
@@ -53,7 +56,7 @@ describe('Native ETH Integration Tests', async () => {
 
   describe('estimateGas', () => {
     it('Should estimate gas for ETH transfer', async () => {
-      const amount = utils.parseEther('0.5')
+      const amount = utils.parseEther('0.0000001')
       const addr = '0x' + '1234'.repeat(10)
       const gas = await env.ovmEth.estimateGas.transfer(addr, amount)
       // Expect gas to be less than or equal to the target plus 1%
@@ -61,7 +64,7 @@ describe('Native ETH Integration Tests', async () => {
     })
 
     it('Should estimate gas for ETH withdraw', async () => {
-      const amount = utils.parseEther('0.5')
+      const amount = utils.parseEther('0.0000001')
       const gas = await env.l2Bridge.estimateGas.withdraw(
         predeploys.OVM_ETH,
         amount,
@@ -186,14 +189,13 @@ describe('Native ETH Integration Tests', async () => {
     await expect(
       env.l1Bridge.depositETH(DEFAULT_TEST_GAS_L2, data, {
         value: depositAmount,
-        gasLimit: 4_000_000,
       })
-    ).to.be.revertedWith(
-      'Transaction data size exceeds maximum for rollup transaction.'
-    )
+    ).to.be.reverted
   })
 
-  it('withdraw', async () => {
+  it('withdraw', async function() {
+    await useDynamicTimeoutForWithdrawals(this, env)
+
     const withdrawAmount = BigNumber.from(3)
     const preBalances = await getBalances(env)
     expect(
@@ -201,31 +203,43 @@ describe('Native ETH Integration Tests', async () => {
       'Cannot run withdrawal test before any deposits...'
     )
 
+    const transaction = await env.l2Bridge.withdraw(
+      predeploys.OVM_ETH,
+      withdrawAmount,
+      DEFAULT_TEST_GAS_L2,
+      '0xFFFF'
+    )
+    await transaction.wait()
+    await env.relayXDomainMessages(transaction)
     const receipts = await env.waitForXDomainTransaction(
-      env.l2Bridge.withdraw(
-        predeploys.OVM_ETH,
-        withdrawAmount,
-        DEFAULT_TEST_GAS_L2,
-        '0xFFFF'
-      ),
+      transaction,
       Direction.L2ToL1
     )
     const fee = receipts.tx.gasLimit.mul(receipts.tx.gasPrice)
 
     const postBalances = await getBalances(env)
 
-    expect(postBalances.l1BridgeBalance).to.deep.eq(
-      preBalances.l1BridgeBalance.sub(withdrawAmount)
+    // Approximate because there's a fee related to relaying the L2 => L1 message and it throws off the math.
+    expectApprox(
+      postBalances.l1BridgeBalance,
+      preBalances.l1BridgeBalance.sub(withdrawAmount),
+      1
     )
-    expect(postBalances.l2UserBalance).to.deep.eq(
-      preBalances.l2UserBalance.sub(withdrawAmount.add(fee))
+    expectApprox(
+      postBalances.l2UserBalance,
+      preBalances.l2UserBalance.sub(withdrawAmount.add(fee)),
+      1
     )
-    expect(postBalances.l1UserBalance).to.deep.eq(
-      preBalances.l1UserBalance.add(withdrawAmount)
+    expectApprox(
+      postBalances.l1UserBalance,
+      preBalances.l1UserBalance.add(withdrawAmount),
+      1
     )
   })
 
-  it('withdrawTo', async () => {
+  it('withdrawTo', async function() {
+    await useDynamicTimeoutForWithdrawals(this, env)
+
     const withdrawAmount = BigNumber.from(3)
 
     const preBalances = await getBalances(env)
@@ -235,14 +249,17 @@ describe('Native ETH Integration Tests', async () => {
       'Cannot run withdrawal test before any deposits...'
     )
 
+    const transaction = await env.l2Bridge.withdrawTo(
+      predeploys.OVM_ETH,
+      l1Bob.address,
+      withdrawAmount,
+      DEFAULT_TEST_GAS_L2,
+      '0xFFFF'
+    )
+    await transaction.wait()
+    await env.relayXDomainMessages(transaction)
     const receipts = await env.waitForXDomainTransaction(
-      env.l2Bridge.withdrawTo(
-        predeploys.OVM_ETH,
-        l1Bob.address,
-        withdrawAmount,
-        DEFAULT_TEST_GAS_L2,
-        '0xFFFF'
-      ),
+      transaction,
       Direction.L2ToL1
     )
     const fee = receipts.tx.gasLimit.mul(receipts.tx.gasPrice)
@@ -260,7 +277,9 @@ describe('Native ETH Integration Tests', async () => {
     )
   })
 
-  it('deposit, transfer, withdraw', async () => {
+  it('deposit, transfer, withdraw', async function() {
+    await useDynamicTimeoutForWithdrawals(this, env)
+
     // 1. deposit
     const amount = utils.parseEther('1')
     await env.waitForXDomainTransaction(
@@ -282,15 +301,18 @@ describe('Native ETH Integration Tests', async () => {
 
     // 3. do withdrawal
     const withdrawnAmount = utils.parseEther('0.95')
+    const transaction = await env.l2Bridge
+      .connect(other)
+      .withdraw(
+        predeploys.OVM_ETH,
+        withdrawnAmount,
+        DEFAULT_TEST_GAS_L1,
+        '0xFFFF'
+      )
+    await transaction.wait()
+    await env.relayXDomainMessages(transaction)
     const receipts = await env.waitForXDomainTransaction(
-      env.l2Bridge
-        .connect(other)
-        .withdraw(
-          predeploys.OVM_ETH,
-          withdrawnAmount,
-          DEFAULT_TEST_GAS_L1,
-          '0xFFFF'
-        ),
+      transaction,
       Direction.L2ToL1
     )
 

integration-tests/test/ovmcontext.spec.ts
@@ -1,15 +1,14 @@
+import { expect } from 'chai'
+
+/* Imports: External */
 import { ethers } from 'hardhat'
 import { injectL2Context } from '@eth-optimism/core-utils'
-import { expect } from 'chai'
-import {
-  sleep,
-  l2Provider,
-  l1Provider,
-  getAddressManager,
-} from './shared/utils'
+import { Contract, BigNumber } from 'ethers'
+
+/* Imports: Internal */
+import { l2Provider, l1Provider, IS_PROD_NETWORK } from './shared/utils'
 import { OptimismEnv } from './shared/env'
-import { getContractFactory } from '@eth-optimism/contracts'
-import { Contract, ContractFactory, Wallet, BigNumber } from 'ethers'
+import { Direction } from './shared/watcher-utils'
 
 /**
  * These tests cover the OVM execution contexts. In the OVM execution
@@ -17,73 +16,50 @@ import { Contract, ContractFactory, Wallet, BigNumber } from 'ethers'
  * must be equal to the blocknumber/timestamp of the L1 transaction.
  */
 describe('OVM Context: Layer 2 EVM Context', () => {
-  let address: string
-  let CanonicalTransactionChain: Contract
+  const L2Provider = injectL2Context(l2Provider)
+  let env: OptimismEnv
+  before(async () => {
+    env = await OptimismEnv.new()
+  })
+
   let OVMMulticall: Contract
   let OVMContextStorage: Contract
-
-  const L1Provider = l1Provider
-  const L2Provider = injectL2Context(l2Provider)
-
-  before(async () => {
-    const env = await OptimismEnv.new()
-    // Create providers and signers
-    const l1Wallet = env.l1Wallet
-    const l2Wallet = env.l2Wallet
-    const addressManager = env.addressManager
-
-    // deploy the contract
+  beforeEach(async () => {
     const OVMContextStorageFactory = await ethers.getContractFactory(
       'OVMContextStorage',
-      l2Wallet
+      env.l2Wallet
+    )
+    const OVMMulticallFactory = await ethers.getContractFactory(
+      'OVMMulticall',
+      env.l2Wallet
     )
 
     OVMContextStorage = await OVMContextStorageFactory.deploy()
-    const receipt = await OVMContextStorage.deployTransaction.wait()
-    address = OVMContextStorage.address
-
-    const ctcAddress = await addressManager.getAddress(
-      'OVM_CanonicalTransactionChain'
-    )
-    const CanonicalTransactionChainFactory = getContractFactory(
-      'OVM_CanonicalTransactionChain'
-    )
-
-    CanonicalTransactionChain = CanonicalTransactionChainFactory.connect(
-      l1Wallet
-    ).attach(ctcAddress)
-
-    const OVMMulticallFactory = await ethers.getContractFactory(
-      'OVMMulticall',
-      l2Wallet
-    )
-
+    await OVMContextStorage.deployTransaction.wait()
     OVMMulticall = await OVMMulticallFactory.deploy()
     await OVMMulticall.deployTransaction.wait()
   })
 
-  it('Enqueue: `block.number` and `block.timestamp` have L1 values', async () => {
-    for (let i = 0; i < 5; i++) {
-      const l2Tip = await L2Provider.getBlock('latest')
-      const tx = await CanonicalTransactionChain.enqueue(
-        OVMContextStorage.address,
-        500_000,
-        '0x'
-      )
+  let numTxs = 5
+  if (IS_PROD_NETWORK) {
+    // Tests take way too long if we don't reduce the number of txs here.
+    numTxs = 1
+  }
 
-      // Wait for the enqueue to be ingested
-      while (true) {
-        const tip = await L2Provider.getBlock('latest')
-        if (tip.number === l2Tip.number + 1) {
-          break
-        }
-        await sleep(500)
-      }
+  it('enqueue: `block.number` and `block.timestamp` have L1 values', async () => {
+    for (let i = 0; i < numTxs; i++) {
+      const tx = await env.l1Messenger.sendMessage(
+        OVMContextStorage.address,
+        '0x',
+        2_000_000
+      )
+      const receipt = await tx.wait()
 
       // Get the receipt
-      const receipt = await tx.wait()
       // The transaction did not revert
       expect(receipt.status).to.equal(1)
+
+      await env.waitForXDomainTransaction(tx, Direction.L1ToL2)
 
       // Get the L1 block that the enqueue transaction was in so that
       // the timestamp can be compared against the layer two contract
@@ -100,11 +76,15 @@ describe('OVM Context: Layer 2 EVM Context', () => {
   })
 
   it('should set correct OVM Context for `eth_call`', async () => {
-    const tip = await L2Provider.getBlockWithTransactions('latest')
-    const start = Math.max(0, tip.number - 5)
+    for (let i = 0; i < numTxs; i++) {
+      // Make an empty transaction to bump the latest block number.
+      const dummyTx = await env.l2Wallet.sendTransaction({
+        to: `0x${'11'.repeat(20)}`,
+        data: '0x',
+      })
+      await dummyTx.wait()
 
-    for (let i = start; i < tip.number; i++) {
-      const block = await L2Provider.getBlockWithTransactions(i)
+      const block = await L2Provider.getBlockWithTransactions('latest')
       const [, returnData] = await OVMMulticall.callStatic.aggregate(
         [
           [
@@ -118,7 +98,7 @@ describe('OVM Context: Layer 2 EVM Context', () => {
             OVMMulticall.interface.encodeFunctionData('getCurrentBlockNumber'),
           ],
         ],
-        { blockTag: i }
+        { blockTag: block.number }
       )
 
       const timestamp = BigNumber.from(returnData[0])

integration-tests/test/shared/utils.ts
@@ -1,13 +1,6 @@
 import { expect } from 'chai'
 
-import { Direction, waitForXDomainTransaction } from './watcher-utils'
-
-import {
-  getContractFactory,
-  getContractInterface,
-  predeploys,
-} from '@eth-optimism/contracts'
-import { injectL2Context, remove0x, Watcher } from '@eth-optimism/core-utils'
+/* Imports: External */
 import {
   Contract,
   Wallet,
@@ -17,9 +10,23 @@ import {
   BigNumber,
   utils,
 } from 'ethers'
-import { cleanEnv, str, num } from 'envalid'
+import {
+  getContractFactory,
+  getContractInterface,
+  predeploys,
+} from '@eth-optimism/contracts'
+import { injectL2Context, remove0x, Watcher } from '@eth-optimism/core-utils'
+import { cleanEnv, str, num, bool } from 'envalid'
+import dotenv from 'dotenv'
+
+/* Imports: Internal */
+import { Direction, waitForXDomainTransaction } from './watcher-utils'
 
 export const GWEI = BigNumber.from(1e9)
+
+if (process.env.IS_PROD_NETWORK === 'true') {
+  dotenv.config()
+}
 
 const env = cleanEnv(process.env, {
   L1_URL: str({ default: 'http://localhost:9545' }),
@@ -37,6 +44,8 @@ const env = cleanEnv(process.env, {
   ADDRESS_MANAGER: str({
     default: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   }),
+  L2_CHAINID: num({ default: 420 }),
+  IS_PROD_NETWORK: bool({ default: false }),
 })
 
 // The hardhat instance
@@ -63,6 +72,9 @@ export const l2Wallet = l1Wallet.connect(l2Provider)
 export const PROXY_SEQUENCER_ENTRYPOINT_ADDRESS =
   '0x4200000000000000000000000000000000000004'
 export const OVM_ETH_ADDRESS = predeploys.OVM_ETH
+
+export const L2_CHAINID = env.L2_CHAINID
+export const IS_PROD_NETWORK = env.IS_PROD_NETWORK
 
 export const getAddressManager = (provider: any) => {
   return getContractFactory('Lib_AddressManager')

web3go-xyz/din-mainnet

integration-tests/test/native-eth.spec.ts
@@ -1,7 +1,10 @@
+import { expect } from 'chai'
+
+/* Imports: External */
+import { Wallet, utils, BigNumber } from 'ethers'
 import { predeploys } from '@eth-optimism/contracts'
 
-import { expect } from 'chai'
-import { Wallet, utils, BigNumber } from 'ethers'
+/* Imports: Internal */
 import { Direction } from './shared/watcher-utils'
 
 import {
@@ -9,7 +12,7 @@ import {
   fundUser,
   PROXY_SEQUENCER_ENTRYPOINT_ADDRESS,
 } from './shared/utils'
-import { OptimismEnv } from './shared/env'
+import { OptimismEnv, useDynamicTimeoutForWithdrawals } from './shared/env'
 
 const DEFAULT_TEST_GAS_L1 = 330_000
 const DEFAULT_TEST_GAS_L2 = 1_300_000
@@ -53,7 +56,7 @@ describe('Native ETH Integration Tests', async () => {
 
   describe('estimateGas', () => {
     it('Should estimate gas for ETH transfer', async () => {
-      const amount = utils.parseEther('0.5')
+      const amount = utils.parseEther('0.0000001')
       const addr = '0x' + '1234'.repeat(10)
       const gas = await env.ovmEth.estimateGas.transfer(addr, amount)
       // Expect gas to be less than or equal to the target plus 1%
@@ -61,7 +64,7 @@ describe('Native ETH Integration Tests', async () => {
     })
 
     it('Should estimate gas for ETH withdraw', async () => {
-      const amount = utils.parseEther('0.5')
+      const amount = utils.parseEther('0.0000001')
       const gas = await env.l2Bridge.estimateGas.withdraw(
         predeploys.OVM_ETH,
         amount,
@@ -186,14 +189,13 @@ describe('Native ETH Integration Tests', async () => {
     await expect(
       env.l1Bridge.depositETH(DEFAULT_TEST_GAS_L2, data, {
         value: depositAmount,
-        gasLimit: 4_000_000,
       })
-    ).to.be.revertedWith(
-      'Transaction data size exceeds maximum for rollup transaction.'
-    )
+    ).to.be.reverted
   })
 
-  it('withdraw', async () => {
+  it('withdraw', async function () {
+    await useDynamicTimeoutForWithdrawals(this, env)
+
     const withdrawAmount = BigNumber.from(3)
     const preBalances = await getBalances(env)
     expect(
@@ -201,31 +203,43 @@ describe('Native ETH Integration Tests', async () => {
       'Cannot run withdrawal test before any deposits...'
     )
 
+    const transaction = await env.l2Bridge.withdraw(
+      predeploys.OVM_ETH,
+      withdrawAmount,
+      DEFAULT_TEST_GAS_L2,
+      '0xFFFF'
+    )
+    await transaction.wait()
+    await env.relayXDomainMessages(transaction)
     const receipts = await env.waitForXDomainTransaction(
-      env.l2Bridge.withdraw(
-        predeploys.OVM_ETH,
-        withdrawAmount,
-        DEFAULT_TEST_GAS_L2,
-        '0xFFFF'
-      ),
+      transaction,
       Direction.L2ToL1
     )
     const fee = receipts.tx.gasLimit.mul(receipts.tx.gasPrice)
 
     const postBalances = await getBalances(env)
 
-    expect(postBalances.l1BridgeBalance).to.deep.eq(
-      preBalances.l1BridgeBalance.sub(withdrawAmount)
+    // Approximate because there's a fee related to relaying the L2 => L1 message and it throws off the math.
+    expectApprox(
+      postBalances.l1BridgeBalance,
+      preBalances.l1BridgeBalance.sub(withdrawAmount),
+      { upperPercentDeviation: 1 }
     )
-    expect(postBalances.l2UserBalance).to.deep.eq(
-      preBalances.l2UserBalance.sub(withdrawAmount.add(fee))
+    expectApprox(
+      postBalances.l2UserBalance,
+      preBalances.l2UserBalance.sub(withdrawAmount.add(fee)),
+      { upperPercentDeviation: 1 }
     )
-    expect(postBalances.l1UserBalance).to.deep.eq(
-      preBalances.l1UserBalance.add(withdrawAmount)
+    expectApprox(
+      postBalances.l1UserBalance,
+      preBalances.l1UserBalance.add(withdrawAmount),
+      { upperPercentDeviation: 1 }
     )
   })
 
-  it('withdrawTo', async () => {
+  it('withdrawTo', async function () {
+    await useDynamicTimeoutForWithdrawals(this, env)
+
     const withdrawAmount = BigNumber.from(3)
 
     const preBalances = await getBalances(env)
@@ -235,14 +249,17 @@ describe('Native ETH Integration Tests', async () => {
       'Cannot run withdrawal test before any deposits...'
     )
 
+    const transaction = await env.l2Bridge.withdrawTo(
+      predeploys.OVM_ETH,
+      l1Bob.address,
+      withdrawAmount,
+      DEFAULT_TEST_GAS_L2,
+      '0xFFFF'
+    )
+    await transaction.wait()
+    await env.relayXDomainMessages(transaction)
     const receipts = await env.waitForXDomainTransaction(
-      env.l2Bridge.withdrawTo(
-        predeploys.OVM_ETH,
-        l1Bob.address,
-        withdrawAmount,
-        DEFAULT_TEST_GAS_L2,
-        '0xFFFF'
-      ),
+      transaction,
       Direction.L2ToL1
     )
     const fee = receipts.tx.gasLimit.mul(receipts.tx.gasPrice)
@@ -260,7 +277,9 @@ describe('Native ETH Integration Tests', async () => {
     )
   })
 
-  it('deposit, transfer, withdraw', async () => {
+  it('deposit, transfer, withdraw', async function () {
+    await useDynamicTimeoutForWithdrawals(this, env)
+
     // 1. deposit
     const amount = utils.parseEther('1')
     await env.waitForXDomainTransaction(
@@ -282,15 +301,18 @@ describe('Native ETH Integration Tests', async () => {
 
     // 3. do withdrawal
     const withdrawnAmount = utils.parseEther('0.95')
+    const transaction = await env.l2Bridge
+      .connect(other)
+      .withdraw(
+        predeploys.OVM_ETH,
+        withdrawnAmount,
+        DEFAULT_TEST_GAS_L1,
+        '0xFFFF'
+      )
+    await transaction.wait()
+    await env.relayXDomainMessages(transaction)
     const receipts = await env.waitForXDomainTransaction(
-      env.l2Bridge
-        .connect(other)
-        .withdraw(
-          predeploys.OVM_ETH,
-          withdrawnAmount,
-          DEFAULT_TEST_GAS_L1,
-          '0xFFFF'
-        ),
+      transaction,
       Direction.L2ToL1
     )
 

integration-tests/test/ovmcontext.spec.ts
@@ -1,15 +1,14 @@
+import { expect } from 'chai'
+
+/* Imports: External */
 import { ethers } from 'hardhat'
 import { injectL2Context } from '@eth-optimism/core-utils'
-import { expect } from 'chai'
-import {
-  sleep,
-  l2Provider,
-  l1Provider,
-  getAddressManager,
-} from './shared/utils'
+import { Contract, BigNumber } from 'ethers'
+
+/* Imports: Internal */
+import { l2Provider, l1Provider, IS_LIVE_NETWORK } from './shared/utils'
 import { OptimismEnv } from './shared/env'
-import { getContractFactory } from '@eth-optimism/contracts'
-import { Contract, ContractFactory, Wallet, BigNumber } from 'ethers'
+import { Direction } from './shared/watcher-utils'
 
 /**
  * These tests cover the OVM execution contexts. In the OVM execution
@@ -17,72 +16,50 @@ import { Contract, ContractFactory, Wallet, BigNumber } from 'ethers'
  * must be equal to the blocknumber/timestamp of the L1 transaction.
  */
 describe('OVM Context: Layer 2 EVM Context', () => {
-  let address: string
-  let CanonicalTransactionChain: Contract
+  const L2Provider = injectL2Context(l2Provider)
+  let env: OptimismEnv
+  before(async () => {
+    env = await OptimismEnv.new()
+  })
+
   let OVMMulticall: Contract
   let OVMContextStorage: Contract
-
-  const L1Provider = l1Provider
-  const L2Provider = injectL2Context(l2Provider)
-
-  before(async () => {
-    const env = await OptimismEnv.new()
-    // Create providers and signers
-    const l1Wallet = env.l1Wallet
-    const l2Wallet = env.l2Wallet
-    const addressManager = env.addressManager
-
-    // deploy the contract
+  beforeEach(async () => {
     const OVMContextStorageFactory = await ethers.getContractFactory(
       'OVMContextStorage',
-      l2Wallet
+      env.l2Wallet
+    )
+    const OVMMulticallFactory = await ethers.getContractFactory(
+      'OVMMulticall',
+      env.l2Wallet
     )
 
     OVMContextStorage = await OVMContextStorageFactory.deploy()
-    const receipt = await OVMContextStorage.deployTransaction.wait()
-    address = OVMContextStorage.address
-
-    const ctcAddress = await addressManager.getAddress(
-      'OVM_CanonicalTransactionChain'
-    )
-    const CanonicalTransactionChainFactory = getContractFactory(
-      'OVM_CanonicalTransactionChain'
-    )
-
-    CanonicalTransactionChain =
-      CanonicalTransactionChainFactory.connect(l1Wallet).attach(ctcAddress)
-
-    const OVMMulticallFactory = await ethers.getContractFactory(
-      'OVMMulticall',
-      l2Wallet
-    )
-
+    await OVMContextStorage.deployTransaction.wait()
     OVMMulticall = await OVMMulticallFactory.deploy()
     await OVMMulticall.deployTransaction.wait()
   })
 
-  it('Enqueue: `block.number` and `block.timestamp` have L1 values', async () => {
-    for (let i = 0; i < 5; i++) {
-      const l2Tip = await L2Provider.getBlock('latest')
-      const tx = await CanonicalTransactionChain.enqueue(
-        OVMContextStorage.address,
-        500_000,
-        '0x'
-      )
+  let numTxs = 5
+  if (IS_LIVE_NETWORK) {
+    // Tests take way too long if we don't reduce the number of txs here.
+    numTxs = 1
+  }
 
-      // Wait for the enqueue to be ingested
-      while (true) {
-        const tip = await L2Provider.getBlock('latest')
-        if (tip.number === l2Tip.number + 1) {
-          break
-        }
-        await sleep(500)
-      }
+  it('enqueue: `block.number` and `block.timestamp` have L1 values', async () => {
+    for (let i = 0; i < numTxs; i++) {
+      const tx = await env.l1Messenger.sendMessage(
+        OVMContextStorage.address,
+        '0x',
+        2_000_000
+      )
+      const receipt = await tx.wait()
 
       // Get the receipt
-      const receipt = await tx.wait()
       // The transaction did not revert
       expect(receipt.status).to.equal(1)
+
+      await env.waitForXDomainTransaction(tx, Direction.L1ToL2)
 
       // Get the L1 block that the enqueue transaction was in so that
       // the timestamp can be compared against the layer two contract
@@ -96,14 +73,18 @@ describe('OVM Context: Layer 2 EVM Context', () => {
       const timestamp = await OVMContextStorage.timestamps(i)
       expect(block.timestamp).to.deep.equal(timestamp.toNumber())
     }
-  })
+  }).timeout(150000) // this specific test takes a while because it involves L1 to L2 txs
 
   it('should set correct OVM Context for `eth_call`', async () => {
-    const tip = await L2Provider.getBlockWithTransactions('latest')
-    const start = Math.max(0, tip.number - 5)
+    for (let i = 0; i < numTxs; i++) {
+      // Make an empty transaction to bump the latest block number.
+      const dummyTx = await env.l2Wallet.sendTransaction({
+        to: `0x${'11'.repeat(20)}`,
+        data: '0x',
+      })
+      await dummyTx.wait()
 
-    for (let i = start; i < tip.number; i++) {
-      const block = await L2Provider.getBlockWithTransactions(i)
+      const block = await L2Provider.getBlockWithTransactions('latest')
       const [, returnData] = await OVMMulticall.callStatic.aggregate(
         [
           [
@@ -117,7 +98,7 @@ describe('OVM Context: Layer 2 EVM Context', () => {
             OVMMulticall.interface.encodeFunctionData('getCurrentBlockNumber'),
           ],
         ],
-        { blockTag: i }
+        { blockTag: block.number }
       )
 
       const timestamp = BigNumber.from(returnData[0])

integration-tests/test/shared/utils.ts
@@ -1,13 +1,6 @@
 import { expect } from 'chai'
 
-import { Direction, waitForXDomainTransaction } from './watcher-utils'
-
-import {
-  getContractFactory,
-  getContractInterface,
-  predeploys,
-} from '@eth-optimism/contracts'
-import { injectL2Context, remove0x, Watcher } from '@eth-optimism/core-utils'
+/* Imports: External */
 import {
   Contract,
   Wallet,
@@ -17,9 +10,23 @@ import {
   BigNumber,
   utils,
 } from 'ethers'
-import { cleanEnv, str, num } from 'envalid'
+import {
+  getContractFactory,
+  getContractInterface,
+  predeploys,
+} from '@eth-optimism/contracts'
+import { injectL2Context, remove0x, Watcher } from '@eth-optimism/core-utils'
+import { cleanEnv, str, num, bool } from 'envalid'
+import dotenv from 'dotenv'
+
+/* Imports: Internal */
+import { Direction, waitForXDomainTransaction } from './watcher-utils'
 
 export const GWEI = BigNumber.from(1e9)
+
+if (process.env.IS_LIVE_NETWORK === 'true') {
+  dotenv.config()
+}
 
 const env = cleanEnv(process.env, {
   L1_URL: str({ default: 'http://localhost:9545' }),
@@ -37,6 +44,8 @@ const env = cleanEnv(process.env, {
   ADDRESS_MANAGER: str({
     default: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   }),
+  L2_CHAINID: num({ default: 420 }),
+  IS_LIVE_NETWORK: bool({ default: false }),
 })
 
 // The hardhat instance
@@ -63,6 +72,9 @@ export const l2Wallet = l1Wallet.connect(l2Provider)
 export const PROXY_SEQUENCER_ENTRYPOINT_ADDRESS =
   '0x4200000000000000000000000000000000000004'
 export const OVM_ETH_ADDRESS = predeploys.OVM_ETH
+
+export const L2_CHAINID = env.L2_CHAINID
+export const IS_LIVE_NETWORK = env.IS_LIVE_NETWORK
 
 export const getAddressManager = (provider: any) => {
   return getContractFactory('Lib_AddressManager')