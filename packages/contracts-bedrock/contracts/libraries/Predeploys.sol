xenowits/optimism

predeploys: add L2 `ProxyAdmin`
We need an instance of the `ProxyAdmin` on L2 to manage each
of the other predeploys. This change ranges across the entire
codebase and the spec is updated with the new predeploy.

packages/contracts-bedrock/contracts/libraries/Predeploys.sol
@@ -65,4 +65,9 @@ library Predeploys {
      *         instead, which exposes more information about the L1 state.
      */
     address internal constant L1_BLOCK_NUMBER = 0x4200000000000000000000000000000000000013;
+
+    /**
+     * @notice Address of the ProxyAdmin predeploy.
+     */
+    address internal constant PROXY_ADMIN = 0x4200000000000000000000000000000000000016;
 }

web3go-xyz/din-mainnet

packages/contracts-bedrock/contracts/libraries/Predeploys.sol
@@ -89,4 +89,9 @@ library Predeploys {
      *         L2ToL1MessagePasser contract instead.
      */
     address internal constant LEGACY_MESSAGE_PASSER = 0x4200000000000000000000000000000000000000;
+
+    /**
+     * @notice Address of the ProxyAdmin predeploy.
+     */
+    address internal constant PROXY_ADMIN = 0x4200000000000000000000000000000000000018;
 }