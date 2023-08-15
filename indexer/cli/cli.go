ethereum-optimism/optimism

feat(indexer): Hook up api to cli app

indexer/cli/cli.go
@@ -3,8 +3,11 @@ package cli
 import (
 	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/ethereum-optimism/optimism/indexer/api"
 	"github.com/ethereum-optimism/optimism/indexer/config"
+	"github.com/ethereum-optimism/optimism/indexer/database"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/urfave/cli/v2"
@@ -32,6 +35,11 @@ func runIndexer(ctx *cli.Context) error {
 	return nil
 }
 
+// Maybe make NewDB take a config.DBConfig instead of a string in future cleanup
+func getDsn(dbConf config.DBConfig) string {
+	return fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", dbConf.User, dbConf.Password, dbConf.Host, dbConf.Port, dbConf.Name)
+}
+
 func runApi(ctx *cli.Context) error {
 	configPath := ctx.String(ConfigFlag.Name)
 	conf, err := config.LoadConfig(configPath)
@@ -41,8 +49,16 @@ func runApi(ctx *cli.Context) error {
 	if err != nil {
 		log.Crit("Failed to load config", "message", err)
 	}
-	// finish me
-	return nil
+
+	db, err := database.NewDB(getDsn(conf.DB))
+
+	if err != nil {
+		log.Crit("Failed to connect to database", "message", err)
+	}
+
+	server := api.NewApi(db.Bridge)
+
+	return server.Listen(strconv.Itoa(conf.API.Port))
 }
 
 var (

web3go-xyz/din-mainnet

indexer/cli/cli.go
@@ -3,13 +3,14 @@ package cli
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/ethereum-optimism/optimism/indexer"
+	"github.com/ethereum-optimism/optimism/indexer/api"
 	"github.com/ethereum-optimism/optimism/indexer/config"
 	"github.com/ethereum-optimism/optimism/indexer/database"
 	"github.com/ethereum-optimism/optimism/op-service/log"
 	"github.com/ethereum-optimism/optimism/op-service/opio"
-
 	"github.com/ethereum/go-ethereum/params"
 
 	"github.com/urfave/cli/v2"
@@ -69,10 +70,15 @@ func runApi(ctx *cli.Context) error {
 		return err
 	}
 
-	fmt.Println(cfg)
+	db, err := database.NewDB(cfg.DB)
 
-	// finish me
-	return err
+	if err != nil {
+		logger.Crit("Failed to connect to database", "err", err)
+	}
+
+	server := api.NewApi(db.BridgeTransfers, logger)
+
+	return server.Listen(strconv.Itoa(cfg.API.Port))
 }
 
 var (
@@ -92,7 +98,7 @@ func (c *Cli) Run(args []string) error {
 }
 
 func NewCli(GitVersion string, GitCommit string, GitDate string) *Cli {
-	flags := append([]cli.Flag{ConfigFlag}, log.CLIFlags("INDEXER")...)
+	flags := []cli.Flag{ConfigFlag}
 	app := &cli.App{
 		Version:     fmt.Sprintf("%s-%s", GitVersion, params.VersionWithCommit(GitCommit, GitDate)),
 		Description: "An indexer of all optimism events with a serving api layer",